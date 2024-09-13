1005b3d860b0bc7ee6b2fe11b92ff789abbf2bd6
Mia Dinh <miadinh38@gmail.com>
2024-09-13 00:13:13 -0400

    Fix config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"skillhat.ca",
-                port:"",
-                pathname:"/**"
-            },
-        ]
-    }
+  distDir: "build",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "skillhat.ca",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
