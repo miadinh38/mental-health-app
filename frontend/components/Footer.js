bb18559d0ca35f752b57b8f017dbd9d213a8171f
Mia Dinh <miadinh38@gmail.com>
2024-09-20 01:57:12 -0400

    Implement Footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,4 +1,10 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexCenter mb-24 mt-5">
+      <p className="regular-14 w-full text-center text-gray-30">
+        2024 TeenVent | All rights reserved
+      </p>
+    </footer>
+  );
 };
 export default Footer;
