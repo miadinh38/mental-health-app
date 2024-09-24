c2685a8eceef7f785d12ab71fbfbde82e6d3aac8
Mia Dinh <miadinh38@gmail.com>
2024-09-23 23:47:41 -0400

    Add validations and update form

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,33 +1,114 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const defaultInputCheck = {
+    validEmail: true,
+    validPassword: true,
+  };
+
+  const [checkInputValid, setCheckInputValid] = useState(defaultInputCheck);
+
+  const isValidInputs = () => {
+    const { email, password } = formData;
+    if (!email) {
+      toast.error("Email is required");
+      setCheckInputValid({ ...defaultInputCheck, validEmail: false });
+      return false;
+    }
+
+    let regx = /\S+@\S+\.\S+/;
+    if (!regx.test(email)) {
+      toast.error("Please enter a valid email address");
+      setCheckInputValid({ ...defaultInputCheck, validEmail: false });
+      return false;
+    }
+
+    if (!password) {
+      toast.error("Password is required");
+      setCheckInputValid({ ...defaultInputCheck, validPassword: false });
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleChangeInput = (e) => {
+    setCheckInputValid(defaultInputCheck);
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    let checkInputValid = isValidInputs();
+    console.log("Login successfully!", formData);
+  };
+
   return (
-    <form className='pt-20 h-dvh'>
+    <form className="pt-20 h-dvh" onSubmit={handleLogin}>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
         <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <p className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Welcome back
             </p>
+
             <div>
               <label className="block mb-2 text-sm font-medium text-gray-900">
                 Your Email
+                <span className="text-red-500 pl-1">*</span>
+                <input
+                  placeholder="JohnDoe@example.com"
+                  className={`${
+                    checkInputValid.validEmail
+                      ? "border-gray-300"
+                      : "border-red-500"
+                  } 
+                  bg-gray-50 border border-gray-300 text-gray-900
+                  sm:text-sm rounded-lg block w-full p-2.5`}
+                  id="email"
+                  name="email"
+                  type="text"
+                  value={formData.email}
+                  onChange={handleChangeInput}
+                  autoComplete="email"
+                />
               </label>
-              <input
-                placeholder="JohnDoe@example.com"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                id="username"
-                type="text"
-              />
             </div>
+
             <div>
               <label className="block mb-2 text-sm font-medium text-gray-900">
                 Password
+                <span className="text-red-500 pl-1">*</span>
+                <input
+                  className={`${
+                    checkInputValid.validEmail
+                      ? "border-gray-300"
+                      : "border-red-500"
+                  } 
+                  bg-gray-50 border border-gray-300 text-gray-900
+                  sm:text-sm rounded-lg block w-full p-2.5`}
+                  placeholder="••••••••"
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleChangeInput}
+                  autoComplete="password"
+                />
               </label>
-              <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                placeholder="••••••••"
-                id="password"
-                type="password"
-              />
             </div>
 
             <button
@@ -36,6 +117,16 @@ const Login = () => {
             >
               Login
             </button>
+
+            <p className="regular-14">
+              Don't have an account yet?
+              <Link
+                href="/register"
+                className="pl-2 text-green-800 hover:underline"
+              >
+                Register here
+              </Link>
+            </p>
           </div>
         </div>
       </div>
