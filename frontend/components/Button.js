9b6d14691bbd189e547c6a28bc8318bb2bb529d2
Mia Dinh <miadinh38@gmail.com>
2024-09-25 21:23:15 -0400

    Implement logout function

--- frontend/components/Button.js
+++ frontend/components/Button.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 
-const Button = ({ type, title, icon, variant, full}) => {
+const Button = ({ type, title, icon, variant, full, onClick}) => {
   return (
     <button
       type={type}
       className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+      onClick={onClick}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24}/>}
       <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,8 +1,17 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+
+  const handleLogout = () => {
+    localStorage.clear();
+    setIsLoggedIn(false);
+  };
+
   return (
     <nav className="flex justify-between items-center text-xl py-2 px-8">
       <Image
@@ -26,17 +35,26 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <ul className="flex gap-3">
-        {["login", "register"].map((button, index) => (
-          <Link key={index} href={`${button}`}>
-            <Button
-              type="button"
-              title={`${button}`}
-              variant="btn_dark_green capitalize"
-            />
-          </Link>
-        ))}
-      </ul>
+      {!isLoggedIn ? (
+        <ul className="flex gap-3">
+          {["login", "register"].map((button, index) => (
+            <Link key={index} href={`${button}`}>
+              <Button
+                type="button"
+                title={`${button}`}
+                variant="btn_dark_green capitalize"
+              />
+            </Link>
+          ))}
+        </ul>
+      ) : (
+        <Button
+          type="button"
+          title="Logout"
+          variant="btn_dark_green capitalize"
+          onClick={handleLogout}
+        />
+      )}
     </nav>
   );
 };
