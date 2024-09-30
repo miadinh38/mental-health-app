068563846f6ba0b95d9d8a4ba6d38c6657723bfc
Mia Dinh <miadinh38@gmail.com>
2024-09-30 02:17:26 -0400

    Setup email config and template for password reset

---
+++ backend/src/utils/emailSender.js
@@ -0,0 +1,24 @@
+import sgMail from '@sendgrid/mail'
+import dotenv from 'dotenv'
+dotenv.config()
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+export const sendEmail = async (to, subject, html) => {
+  const msg = {
+    to: to,
+    from: {
+      name: 'TeenVent',
+      email: 'helpteenstoday@gmail.com',
+    },
+    subject: subject,
+    html: html,
+  }
+
+  try {
+    await sgMail.send(msg)
+    console.log('Email sent')
+  } catch (error) {
+    console.error('Error sending email:', error.response.body)
+  }
+}
