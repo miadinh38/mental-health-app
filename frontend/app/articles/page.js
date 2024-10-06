fdfb4ab3b9320d34a3940f788bf066d7a3a256e2
Mia Dinh <miadinh38@gmail.com>
2024-10-06 00:23:38 -0400

    Implement SearchBar and search function

--- frontend/app/articles/page.js
+++ frontend/app/articles/page.js
@@ -4,34 +4,63 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import SearchBar from "../../components/SearchBar";
 import ArticlesList from "../../components/ArticlesList";
+import ArticlesSearchList from "../../components/ArticlesSearchList";
 import useInsertArticles from "../../hooks/useInsertAriticles";
+import { searchedArticlesService } from "../services/articlesService.js";
 
 const Articles = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [searchedArticles, setSearchedArticles] = useState([]);
   const { allArticles, loading } = useInsertArticles();
 
-  const handleSearchButton = () => {
-    console.log("Clicked Search");
+  const handleSearchButton = async () => {
+    if (!searchInput) return;
+    try {
+      const response = await searchedArticlesService(searchInput);
+
+      if (response.data && response.data.message) {
+        setSearchedArticles([]);
+      }
+
+      setSearchedArticles(response.data);
+      setSearchInput("");
+    } catch (error) {
+      console.error("Error fetching searched articles:", error);
+    }
   };
 
   return (
     <div className="max-container padding-container">
       <p className="flexCenter bold-32 my-5 p-5">Articles & News</p>
 
-      {/* Search */}
-      <SearchBar
-        onChange={(e) => setSearchInput(e.target.value)}
-        value={searchInput}
-        onClickSearch={handleSearchButton}
-      />
+      <div className="flex flexCenter gap-4 m-10">
+        <button
+          type="button"
+          onClick={() => setSearchedArticles([])}
+          className="btn_dark_green rounded-xl "
+        >
+          ALL
+        </button>
 
-      {/* Render all articles */}
+        {/* Search */}
+        <SearchBar
+          onChange={(e) => setSearchInput(e.target.value)}
+          value={searchInput}
+          onClickSearch={handleSearchButton}
+        />
+      </div>
+
+      {/* Render all or searched articles */}
       {loading ? (
         <div className="h-lvh">
           <Skeleton />
         </div>
-      ) : (
+      ) : searchedArticles.length > 0 ? (
+        <ArticlesSearchList allArticles={searchedArticles} />
+      ) : allArticles.length > 0 ? (
         <ArticlesList allArticles={allArticles} />
+      ) : (
+        <p>No articles found.</p>
       )}
     </div>
   );
