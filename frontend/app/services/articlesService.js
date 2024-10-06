0ddca45d36c0d6a62afd210b36d5a01370ba7ed6
Mia Dinh <miadinh38@gmail.com>
2024-10-05 22:03:34 -0400

    Add api calls for article routes

--- frontend/app/services/articlesService.js
+++ frontend/app/services/articlesService.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import { API_URL } from '../../constants/api';
 
+export const insertArticlesService = async() => {
+  return await axios.post(`${API_URL}/articles`);
+}
+
+export const checkArticlesExist = async() => {
+  return await axios.get(`${API_URL}/articles/check-articles`);
+}
+
 export const fetchAllArticlesService = async() => {
   return await axios.get(`${API_URL}/articles`);
 }
