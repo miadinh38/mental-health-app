8ae2c0711c159b7ca274990ba230e9d0a9f6e8e1
Mia Dinh <miadinh38@gmail.com>
2024-10-05 21:58:27 -0400

    Implement getAllArticles and checArticles functionality

--- backend/src/controllers/articlesController.js
+++ backend/src/controllers/articlesController.js
@@ -1,7 +1,46 @@
-import { fetchAndSaveArticles } from "../services/articlesService.js";
+import { fetchAndSaveArticles, getArticles } from "../services/articlesService.js";
+
+export const insertArticles = async(req, res) => {
+  try {
+    await fetchAndSaveArticles()
+    return res.status(200).json({
+      sucess: true,
+      message: "Articles have been inserted to database"
+    });  
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: 'Error from saving articles controller', message: error.message })
+  }
+}
 
 export const fetchAllArticles = async(req, res) => {
   try {
+    const allArticles = await getArticles()
+    return res.status(200).json(allArticles);  
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: 'Error from fetching all articles controller', message: error.message })
+  }
+}
+
+export const checkArticles = async(req, res) => {
+  try {
+    const result = await getArticles()
+    if (result.length > 0 ){
+      return res.status(200).json({ hasArticle: true });
+    } else {
+      return res.status(200).json({ hasArticle: false });
+    }
+  } catch (error) {
+    console.error('Error from checking articles', error.message)
+  }
+}
+
+export const fetchSearchArticles = async(req, res) => {
+  try {
+    const { input } = req.body; 
     const allArticles = await fetchAndSaveArticles()
     return res.status(200).json(allArticles);  
   } catch (error) {
--- backend/src/routes/articlesRoutes.js
+++ backend/src/routes/articlesRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { fetchAllArticles } from '../controllers/articlesController.js';
+import { insertArticles, fetchAllArticles, fetchSearchArticles, checkArticles } from '../controllers/articlesController.js';
 const router = express.Router();
 
+router.post('/', insertArticles)
+router.get('/check-articles', checkArticles)
 router.get('/', fetchAllArticles)
+router.get('/', fetchSearchArticles)
 
 export default router;
