920d5a684256236afae77007512f554330699e69
Mia Dinh <miadinh38@gmail.com>
2024-10-12 17:54:09 -0400

    Update hashed password

--- backend/src/models/db/seeds/01_users.sql
+++ backend/src/models/db/seeds/01_users.sql
@@ -4,7 +4,7 @@ VALUES
   (
     'Test User',
     'user1@gmail.com',
-    'secret',
+    '$2b$10$/4wkn/7B5FLampFTtjT6Iu8EO8tlFE1YWMdpNZjMW2/GhaQl6JNQS',
     'female',
     '2010-05-05',
     '+123456789'
