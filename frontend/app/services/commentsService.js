c9b2ad06021de611c15ced2d5b89cfaa9349c5f6
Mia Dinh <miadinh38@gmail.com>
2024-11-04 20:11:37 -0500

    Call API for update and delete comments func

--- frontend/app/services/commentsService.js
+++ frontend/app/services/commentsService.js
@@ -15,3 +15,17 @@ export const createNewComment = async (token, content, postId) => {
   };
   return axios.post(`${API_URL}/posts/${postId}/comments`, content , { headers });
 };
+
+export const updateComment = async (token, content, postId, commentId) => {
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  return axios.put(`${API_URL}/posts/${postId}/comments/${commentId}`, content , { headers });
+};
+
+export const deleteComment = async (token, postId, commentId) => {
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  return axios.delete(`${API_URL}/posts/${postId}/comments/${commentId}`, { headers });
+};
