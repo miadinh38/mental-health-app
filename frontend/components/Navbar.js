d4391a360c5bd4458256771c2f8b304b7683c362
Mia Dinh <miadinh38@gmail.com>
2024-11-05 13:41:49 -0500

    Update btn & styling for navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -2,55 +2,63 @@
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
-import { useEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
 
 const Navbar = () => {
   const { logout, isAuth, currentUser } = useAuth();
 
   return (
-    <nav className="flex justify-between items-center text-xl py-2 px-8">
-      <Image
-        src="/teenvent-logo.png"
-        alt="Teenvent logo"
-        width={150}
-        height={32}
-        priority
-      />
+    <nav className="flex justify-between items-center py-4 px-24 bg-purple-50">
+      <Link href="/">
+        <Image
+          src="/Mindora-logo.png"
+          alt="Mindora logo"
+          width={150}
+          height={32}
+          priority
+          className="max-w-full"
+        />
+      </Link>
 
-      <ul className="flex flex-1 justify-start h-full pl-16 gap-12">
-        {["home", "about", "articles", "contact", "community"].map((label, index) => (
-          <Link
-            key={index}
-            href={label === "home" ? `/` : `${label}`}
-            className="capitalize regular-18 text-gray-600 flexCenter cursor-point 
+      <ul className="flex flex-1 justify-start h-full gap-8 flexCenter md:hidden">
+        {["articles", "FAQ", "about", "contact", "community"].map(
+          (label, index) => (
+            <Link
+              key={index}
+              href={`${label}`}
+              className="capitalize regular-16 text-purple-900 flexCenter cursor-point 
             transition-all hover:font-bold"
-          >
-            {label}
-          </Link>
-        ))}
+            >
+              {label}
+            </Link>
+          )
+        )}
       </ul>
 
       {!isAuth ? (
-        <ul className="flex gap-3">
-          {["login", "register"].map((button, index) => (
-            <Link key={index} href={`/${button}`}>
+        <ul className="flex gap-2 md:hidden">
+          {["login", "sign up"].map((button, index) => (
+            <Link
+              key={index}
+              href={`/${button === "sign up" ? "signup" : "login"}`}
+            >
               <Button
                 type="button"
                 title={`${button}`}
-                // variant={`${button === "login" ? 'btn_white' : 'btn_green'} capitalize`}
-                variant='btn_green capitalize'
+                variant={`${
+                  button === "login" ? "btn_white" : "btn_purple"
+                } capitalize w-[123px]`}
               />
             </Link>
           ))}
         </ul>
       ) : (
         <>
-          <p className="pr-4 regular-18">{currentUser?.name}</p>
+          <p className="pr-4 regular-16">{currentUser?.name}</p>
           <Button
             type="button"
             title="Logout"
-            variant="btn_green capitalize"
+            variant="btn_purple capitalize"
             onClick={logout}
           />
         </>
