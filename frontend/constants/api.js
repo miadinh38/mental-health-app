602dc63f9e1269d7148ae3295cf96063090a3c8a
Mia Dinh <miadinh38@gmail.com>
2024-11-23 13:35:53 +0700

    Update domain name

--- frontend/constants/api.js
+++ frontend/constants/api.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:8000/api";
-export const API_URL = "https://teenvent-server.vercel.app/api";
+export const API_URL = "https://mindora-server.vercel.app/api";
